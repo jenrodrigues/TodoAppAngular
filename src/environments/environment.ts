3fa527dd5dd823e6c8a17edc6b7c02693a85a069
Jenifer Rodrigues <jen.rodrigues@gmail.com>
2021-05-17 13:21:09 +0100

    Updating Angular and Firebase versions

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,21 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDCGPTiXhgDwQIyOp5iiQv7KvGsN2EIusI",
-    authDomain: "todos-daba8.firebaseapp.com",
-    databaseURL: "https://todos-daba8.firebaseio.com",
-    projectId: "todos-daba8",
-    storageBucket: "todos-daba8.appspot.com",
-    messagingSenderId: "381634863399",
-    appId: "1:381634863399:web:6811d0527a323c5a368902",
-    measurementId: "G-J2CV32GTSY"
+    // apiKey: "AIzaSyDCGPTiXhgDwQIyOp5iiQv7KvGsN2EIusI",
+    // authDomain: "todos-daba8.firebaseapp.com",
+    // databaseURL: "https://todos-daba8.firebaseio.com",
+    // projectId: "todos-daba8",
+    // storageBucket: "todos-daba8.appspot.com",
+    // messagingSenderId: "381634863399",
+    // appId: "1:381634863399:web:6811d0527a323c5a368902",
+    // measurementId: "G-J2CV32GTSY"
+    apiKey: "AIzaSyAhbSitU8SPZTBBMkM_JM_ECrBabPbWFec",
+    authDomain: "todo-bfdad.firebaseapp.com",
+    databaseURL: "https://todo-bfdad.firebaseio.com", //manter essa linha?
+    projectId: "todo-bfdad",
+    storageBucket: "todo-bfdad.appspot.com",
+    messagingSenderId: "654963422499",
+    appId: "1:654963422499:web:abd995f5e63ebb97284896"
   },
 };
 
